5ae932a7ac9603fae2713b87ee52ccffa36a2d47
fahad-cdda <165648235+fahad-cdda@users.noreply.github.com>
2024-08-25 08:28:33 +0600

    aaa

--- api/serializers/users.py
+++ api/serializers/users.py
@@ -5,14 +5,32 @@ from accounts.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = "__all__"
+        fields = '__all__'
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            role=validated_data.get('role'),
+            phone_number=validated_data.get('phone_number'),
+            address=validated_data.get('address'),
+            restaurant=validated_data.get('restaurant')
+        )
         return user
 
     def update(self, instance, validated_data):
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        if 'password' in validated_data:
+            instance.set_password(validated_data['password'])
+        instance.role = validated_data.get('role', instance.role)
+        instance.phone_number = validated_data.get('phone_number', instance.phone_number)
+        instance.address = validated_data.get('address', instance.address)
+        instance.restaurant = validated_data.get('restaurant', instance.restaurant)
         instance.save()
         return instance
+
